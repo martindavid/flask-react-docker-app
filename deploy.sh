2954ca446d554cd15c1bf463719afcf7d18eae19
Martin Valentino <martin.valentino@cultureamp.com>
2019-10-10 11:18:17 +1100

    Add integration with gitlab ci

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+ssh -o StrictHostKeyChecking=no root@$DO_IP_ADDRESS << 'ENDSSH'
+	cd /app
+	export $(cat .env | xargs)
+	docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
+	docker pull $IMAGE:api
+	docker pull $IMAGE:client
+	docker pull $IMAGE:nginx
+	docker-compose -f docker-compose.prod.yml up -d
+ENDSSH
+
